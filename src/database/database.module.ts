1a69e991cfe5e3e0068fc4a47465ca030e7bb4ed
LUVINA\tutuananh <tutuananh@luvina.net>
2024-12-06 15:26:56 +0700

    config auto load entities

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Role } from 'src/modules/role/entities/role.entity';
-import { User } from 'src/modules/user/entities/user.entity';
 
 @Module({
     imports: [
@@ -14,9 +12,9 @@ import { User } from 'src/modules/user/entities/user.entity';
                 username: configService.get<string>('MYSQL_USERNAME'),
                 password: configService.get<string>('MYSQL_PASSWORD'),
                 database: configService.get<string>('MYSQL_DB'),
-                entities: [User, Role],
-                synchronize: true,
-                // logging: true,
+                autoLoadEntities: true,
+                synchronize: true, //should be false at production!
+                logging: false,
             }),
             inject: [ConfigService],
         }),
